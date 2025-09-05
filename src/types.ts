1a50994262d0229ee20e55ddd9798ce7604ce3ed
Valeria Gamboa <valugamboa70@gmail.com>
2025-09-05 12:10:44 -0500

    Crear carpeta nueva

---
+++ src/types.ts
@@ -0,0 +1,25 @@
+
+
+// ------------------------------
+// Tipos básicos y uniones
+// ------------------------------
+export type TipoMovimiento = "entrada" | "salida"; 
+export type Categoria = "alimentos" | "ropa" | "tecnologia" | "otros";
+
+// Union type para el precio
+export type Precio = number | string; 
+
+// ------------------------------
+// Intersección de tipos
+// ------------------------------
+export type InfoBase = {
+  codigo: string;
+  nombre: string;
+};
+
+export type Inventariable = {
+  cantidad: number;
+  precio: Precio;
+};
+
+export type ProductoGenerico = InfoBase & Inventariable & { categoria: Categoria };
